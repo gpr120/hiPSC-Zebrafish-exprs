aeb719651ea828ebcba5946f889ad81eada83d31
k-himeno <47313624+k-himeno@users.noreply.github.com>
2023-07-10 01:03:35 +0900

    適切なクラスタリング手法を指定． クラスタリングしない際の並び替えを指定． tpm値からtmm値に変更

--- keisan/scripts/NGS_analysis_R/230106_z-score_zebra.R
+++ keisan/scripts/NGS_analysis_R/230106_z-score_zebra.R
@@ -45,7 +45,8 @@ gene_heatmap <- function(fData, pData, exp, gene, save, exact=T, type="Gene.Symb
   # 作図
   colnames(zexp) <- fData[x,"Gene.Symbol"]
   pdf(paste(save, ".pdf", sep=""))
-  heatmap.2(t(zexp),col="bluered",trace="none", Rowv=Rowv, Colv=Colv, margins=c(8, 5), cexRow=cexRow)
+  heatmap.2(t(zexp),col="bluered",trace="none", Rowv=Rowv, Colv=Colv, margins=c(8, 5), cexRow=cexRow, distfun = function(x) dist(x, method="euclidean"),
+  hclustfun = function(x) hclust(x, method="ward.D2"))
   dev.off()
 }
 
@@ -64,23 +65,28 @@ setwd("heatmap")
 save <- "HOX genes"
 gene <- c("^hoxa1a", "^hoxa2b", "^hoxa3a", "^hoxa4a", "^hoxa5a" ,"^hoxa9a", "^hoxa9b", "^hoxa10b", "^hoxa11a", "^hoxa11b", "^hoxa13a", "^hoxa13b", "^hoxb1a", "^hoxb1b", "^hoxb2a", "^hoxb3a", "^hoxb4a", "^hoxb5a", "^hoxb5b", "^hoxb6a", "^hoxb6b", "^hoxb7a", "^hoxb8a", "^hoxb8b", "^hoxb9a", "^hoxb10a", "^hoxb13a", "^hoxc1a", "^hoxc3a", "^hoxc4a", "^hoxc5a", "^hoxc6a", "^hoxc6b", "^hoxc8a", "^hoxc9a", "^hoxc10a", "^hoxc11a", "^hoxc11b", "^hoxc12a", "^hoxc12b", "^hoxc13a", "^hoxc13b", "^hoxd3a", "^hoxd4a", "^hoxd9a", "^hoxd10a", "^hoxd11a", "^hoxd12a", "^hoxd13a"
 )
-gene_heatmap(fData, pData, tpm, gene, save, exact=F, Colv=F,cexRow=0.5)
+# gene_heatmap(fData, pData, tpm, gene, save, exact=T, Colv=T,cexRow=0.5)
+# control, BPA, RA_10, RA_12.5, RA_10+BPA の順番にする．手動の方が早い
+# sample_tpm = c(7,8,9,10,11,12,1,2,3,4,5,6,19,20,21,22,23,24,25,26,27,28,29,30,13,14,15,16,17,18)
+sample <- c(25,26,27,28,29,30,1,2,3,4,5,6,13,14,15,16,17,18,19,20,21,22,23,24,7,8,9,10,11,12)
+gene_heatmap(fData, pData, tmm, gene, paste(save, sep="_"), exact=T, Colv=T,cexRow=0.5)
 save <- paste(save, "_No_cluster", sep="")
-gene_heatmap(fData, pData, tpm, gene, save, exact=F, Colv=F, Rowv=F,sample=c(7,8,9,10,11,12,1,2,3,4,5,6,19,20,21,22,23,24,25,26,27,28,29,30,13,14,15,16,17,18), cexRow=0.5)
+gene_heatmap(fData, pData, tmm, gene, save, exact=F, Colv=F, Rowv=T,sample=sample, cexRow=0.5)
 gene_all <- c(gene)
 
 
-gene <- c("^otx1", "^otx2a", "^otx2b", "^egr2a", "^egr2b", "^meis1a", "^meis1b", "^meis2a", "^meis2b","^fgf8a", "^fgf8b")
+gene <- c("^otx1", "^otx2a", "^otx2b", "^egr2a", "^egr2b", "^meis1a", "^meis1b", "^meis2a", "^meis2b","^fgf8a", "^fgf8b", "^fgf3$")
 save <- "HOX related genes"
-gene_heatmap(fData, pData, tpm, gene, save, exact=F, Colv=F)
+gene_heatmap(fData, pData, tmm, gene, save, exact=F, Colv=T)
 save <- paste(save, "_No_cluster", sep="")
-gene_heatmap(fData, pData, tpm, gene, save, exact=F, Colv=F, Rowv=F,sample=c(7,8,9,10,11,12,1,2,3,4,5,6,19,20,21,22,23,24,25,26,27,28,29,30,13,14,15,16,17,18))
+gene_heatmap(fData, pData, tmm, gene, save, exact=F, Colv=F, Rowv=F,sample=sample)
 gene_all <- c(gene_all, gene)
 
+# rargbは検出限界以下のサンプルがあるため，除外
 save <- "Retinoic acid related genes"
-gene <- c("aldh1a2", "aldh1a3", "cyp26a1", "cyp26b1", "cyp26c1 crabp1a", "crabp1b", "crabp2a", "crabp2b", "stra6", "raraa", "rarab", "rarga", "rargb", "rxraa", "rxrab", "rxrba", "rxrbb", "rxrga", "rxrgb")
-gene_heatmap(fData, pData, tpm, gene, save, exact=T, Colv=F)
+gene <- c("aldh1a2", "aldh1a3", "cyp26a1", "cyp26b1", "cyp26c1 crabp1a", "crabp1b", "crabp2a", "crabp2b", "stra6", "raraa", "rarab", "rarga",  "rxraa", "rxrab", "rxrba", "rxrbb", "rxrga", "rxrgb")
+gene_heatmap(fData, pData, tmm, gene, save, exact=T, Colv=T)
 save <- paste(save, "_No_cluster", sep="")
-gene_heatmap(fData, pData, tpm, gene, save, exact=T, Colv=F, Rowv=F,sample=c(7,8,9,10,11,12,1,2,3,4,5,6,19,20,21,22,23,24,25,26,27,28,29,30,13,14,15,16,17,18))
+gene_heatmap(fData, pData, tmm, gene, save, exact=T, Colv=F, Rowv=F,sample=sample)
 gene_all <- c(gene_all, gene)
 
