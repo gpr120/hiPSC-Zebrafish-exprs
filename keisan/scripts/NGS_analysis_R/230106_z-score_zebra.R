f812f3c935912d1722d71c7c6d14adda7897c5ca
k-himeno <47313624+k-himeno@users.noreply.github.com>
2023-09-30 22:16:54 +0900

    Retinoic acid related genesに関するバグ修正

--- keisan/scripts/NGS_analysis_R/230106_z-score_zebra.R
+++ keisan/scripts/NGS_analysis_R/230106_z-score_zebra.R
@@ -61,6 +61,7 @@ tmm <- read.csv("tcc_all_tmm.csv", header=T, row.names=1, check.names=F)
 dir.create("heatmap")
 setwd("heatmap")
 
+sample <- c(25,26,27,28,29,30,1,2,3,4,5,6,13,14,15,16,17,18,19,20,21,22,23,24,7,8,9,10,11,12)
 
 save <- "HOX genes"
 gene <- c("^hoxa1a", "^hoxa2b", "^hoxa3a", "^hoxa4a", "^hoxa5a" ,"^hoxa9a", "^hoxa9b", "^hoxa10b", "^hoxa11a", "^hoxa11b", "^hoxa13a", "^hoxa13b", "^hoxb1a", "^hoxb1b", "^hoxb2a", "^hoxb3a", "^hoxb4a", "^hoxb5a", "^hoxb5b", "^hoxb6a", "^hoxb6b", "^hoxb7a", "^hoxb8a", "^hoxb8b", "^hoxb9a", "^hoxb10a", "^hoxb13a", "^hoxc1a", "^hoxc3a", "^hoxc4a", "^hoxc5a", "^hoxc6a", "^hoxc6b", "^hoxc8a", "^hoxc9a", "^hoxc10a", "^hoxc11a", "^hoxc11b", "^hoxc12a", "^hoxc12b", "^hoxc13a", "^hoxc13b", "^hoxd3a", "^hoxd4a", "^hoxd9a", "^hoxd10a", "^hoxd11a", "^hoxd12a", "^hoxd13a"
@@ -68,7 +69,7 @@ gene <- c("^hoxa1a", "^hoxa2b", "^hoxa3a", "^hoxa4a", "^hoxa5a" ,"^hoxa9a", "^ho
 # gene_heatmap(fData, pData, tpm, gene, save, exact=T, Colv=T,cexRow=0.5)
 # control, BPA, RA_10, RA_12.5, RA_10+BPA の順番にする．手動の方が早い
 # sample_tpm = c(7,8,9,10,11,12,1,2,3,4,5,6,19,20,21,22,23,24,25,26,27,28,29,30,13,14,15,16,17,18)
-sample <- c(25,26,27,28,29,30,1,2,3,4,5,6,13,14,15,16,17,18,19,20,21,22,23,24,7,8,9,10,11,12)
+
 gene_heatmap(fData, pData, tmm, gene, paste(save, sep="_"), exact=T, Colv=T,cexRow=0.5)
 save <- paste(save, "_No_cluster", sep="")
 gene_heatmap(fData, pData, tmm, gene, save, exact=F, Colv=F, Rowv=T,sample=sample, cexRow=0.5)
@@ -84,7 +85,7 @@ gene_all <- c(gene_all, gene)
 
 # rargbは検出限界以下のサンプルがあるため，除外
 save <- "Retinoic acid related genes"
-gene <- c("aldh1a2", "aldh1a3", "cyp26a1", "cyp26b1", "cyp26c1 crabp1a", "crabp1b", "crabp2a", "crabp2b", "stra6", "raraa", "rarab", "rarga",  "rxraa", "rxrab", "rxrba", "rxrbb", "rxrga", "rxrgb")
+gene <- c("aldh1a2", "aldh1a3", "cyp26a1", "cyp26b1", "cyp26c1", "crabp1b", "crabp2a", "crabp2b", "stra6", "raraa", "rarab", "rarga",  "rxraa", "rxrab", "rxrba", "rxrbb", "rxrga", "rxrgb")
 gene_heatmap(fData, pData, tmm, gene, save, exact=T, Colv=T)
 save <- paste(save, "_No_cluster", sep="")
 gene_heatmap(fData, pData, tmm, gene, save, exact=T, Colv=F, Rowv=T,sample=sample)
